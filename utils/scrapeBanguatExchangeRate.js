158eec9df109696b39628bef0b775d2386e83379
Jose Ramirez <jramirez@rocksoftgt.dev>
2024-10-08 23:07:13 -0600

    Feat: Agregada tasa colombia

--- utils/scrapeBanguatExchangeRate.js
+++ utils/scrapeBanguatExchangeRate.js
@@ -3,23 +3,21 @@ const axios = require('axios');
 async function scrapeBanguatExchangeRate() {
     try {
         // Realizar la solicitud HTTP a la página
-        const response = await axios.get('https://www.banguat.gob.gt/tipo_cambio/');
+        const response = await axios.get('https://dolar.wilkinsonpc.com.co');
         const html = response.data;
 
         // Cargar el HTML en cheerio
         const $ = cheerio.load(html);
 
         // Extraer el valor del tipo de cambio
-        const exchangeRate = $('.detalle_banguat td').first().text().trim();
+        const exchangeRate = $('.sube-numero').first().text().replace('$', '').trim();
 
-        // Extraer la fecha del encabezado
-        const dateText = $('.encabezado_banguat th').first().text();
-        const dateMatch = dateText.match(/vigente para el\s+(.+)$/);
-        const date = dateMatch ? dateMatch[1] : 'Fecha no encontrada';
+        // Extraer la fecha del enlace
+        const date = $('p a').first().attr('href').split('/')[1];
 
         return {
             date: date,
-            exchangeRate: parseFloat(exchangeRate) || null
+            exchangeRate: parseFloat(exchangeRate.replace(',', '')) || null
         };
     } catch (error) {
         console.error('Error al obtener la tasa de cambio:', error.message);
