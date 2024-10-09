221650d01aefe33803dbcd5f083aa022a8e4154e
Jose Ramirez <jramirez@rocksoftgt.dev>
2024-10-08 22:41:38 -0600

    Feat: Second commit

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const axios = require('axios');
-const cheerio = require('cheerio');
 const main = require("./utils/main");
 
 const app = express();
@@ -12,26 +10,13 @@ const port = 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
-// Example route
-app.get('/', async (req, res) => {
-    try {
-        const response = await axios.get('https://example.com');
-        const html = response.data;
-        const $ = cheerio.load(html);
-        const title = $('title').text();
-        res.send({title});
-    } catch (error) {
-        res.status(500).send({error: 'Error fetching data'});
-    }
-});
-
 // Define the async function igual
 async function execute(req, res) {
     try {
         const result = await main()
         res.send(result);
     } catch (error) {
-        res.status(500).send({ error: 'Error in function igual' });
+        res.status(500).send({ error: 'Error fetching data' });
     }
 }
 
